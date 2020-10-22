56a345cf69e57bea6dff93b502123509d6cbeb25
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-10-22 16:57:36 -0300

    firmware: Importing the system management routines from OBDH 2.0 #22

---
+++ firmware/system/sys_log/sys_log.c
@@ -0,0 +1,414 @@
+/*
+ * sys_log.c
+ * 
+ * Copyright (C) 2020, SpaceLab.
+ * 
+ * This file is part of EPS 2.0.
+ * 
+ * EPS 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EPS 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with EPS 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief System log implementation.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 2020/10/22
+ * 
+ * \addtogroup sys_log
+ * \{
+ */
+
+#include <math.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <version.h>
+
+#include "sys_log.h"
+#include "sys_log_config.h"
+
+int sys_log_init()
+{
+    if (sys_log_uart_init())
+    {
+        sys_log_new_line();
+
+        sys_log_print_license_msg();
+
+        sys_log_print_splash_screen();
+
+        sys_log_print_msg("===================================================================");
+        sys_log_new_line();
+        sys_log_print_msg("Version:\t");
+        sys_log_print_firmware_version();
+        sys_log_new_line();
+
+        sys_log_print_msg("Status:\t\t");
+        sys_log_print_msg(FIRMWARE_STATUS);
+        sys_log_new_line();
+
+        sys_log_print_msg("Author:\t\t");
+        sys_log_print_msg(FIRMWARE_AUTHOR);
+        sys_log_print_msg(" <");
+        sys_log_print_msg(FIRMWARE_AUTHOR_EMAIL);
+        sys_log_print_msg(">");
+        sys_log_new_line();
+        sys_log_print_msg("===================================================================");
+        sys_log_new_line();
+        sys_log_new_line();
+        sys_log_new_line();
+
+        sys_log_mutex_create();
+
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void sys_log_set_color(uint8_t color)
+{
+    switch(color)
+    {
+        case SYS_LOG_COLOR_BLACK:
+            sys_log_print_msg("\033[1;30m");
+            break;
+        case SYS_LOG_COLOR_RED:
+            sys_log_print_msg("\033[1;31m");
+            break;
+        case SYS_LOG_COLOR_GREEN:
+            sys_log_print_msg("\033[1;32m");
+            break;
+        case SYS_LOG_COLOR_YELLOW:
+            sys_log_print_msg("\033[1;33m");
+            break;
+        case SYS_LOG_COLOR_BLUE:
+            sys_log_print_msg("\033[1;34m");
+            break;
+        case SYS_LOG_COLOR_MAGENTA:
+            sys_log_print_msg("\033[1;35m");
+            break;
+        case SYS_LOG_COLOR_CYAN:
+            sys_log_print_msg("\033[1;36m");
+            break;
+        case SYS_LOG_COLOR_WHITE:
+            sys_log_print_msg("\033[1;37m");
+            break;
+        default:
+            sys_log_reset_color();
+    }
+}
+
+void sys_log_reset_color()
+{
+    sys_log_print_msg("\033[0m");
+}
+
+void sys_log_print_event(uint8_t type, const char *event)
+{
+    sys_log_mutex_take();
+
+    sys_log_print_system_time();
+    sys_log_print_msg(" ");
+
+    switch(type)
+    {
+        case SYS_LOG_INFO:
+            break;
+        case SYS_LOG_WARNING:
+            sys_log_set_color(SYS_LOG_WARNING_COLOR);
+            break;
+        case SYS_LOG_ERROR:
+            sys_log_set_color(SYS_LOG_ERROR_COLOR);
+            break;
+        default:
+            break;
+    }
+
+    sys_log_print_msg(event);
+}
+
+void sys_log_print_event_from_module(uint8_t type, const char *module, const char *event)
+{
+    sys_log_mutex_take();
+
+    sys_log_print_system_time();
+
+    sys_log_set_color(SYS_LOG_MODULE_NAME_COLOR);
+    sys_log_print_msg(" ");
+    sys_log_print_msg(module);
+    sys_log_reset_color();
+    sys_log_print_msg(": ");
+
+    switch(type)
+    {
+        case SYS_LOG_INFO:
+            break;
+        case SYS_LOG_WARNING:
+            sys_log_set_color(SYS_LOG_WARNING_COLOR);
+            break;
+        case SYS_LOG_ERROR:
+            sys_log_set_color(SYS_LOG_ERROR_COLOR);
+            break;
+        default:
+            break;
+    }
+
+    sys_log_print_msg(event);
+}
+
+void sys_log_print_msg(const char *msg)
+{
+    uint16_t i = 0;
+    while(msg[i] != '\0')
+    {
+        sys_log_print_byte(msg[i]);
+        i++;
+    }
+}
+
+void sys_log_new_line()
+{
+    sys_log_reset_color();
+    sys_log_print_msg("\n\r");
+    sys_log_mutex_give();
+}
+
+void sys_log_print_digit(uint8_t digit)
+{
+    if (digit < 0x0A)
+    {
+        sys_log_print_byte(digit + 0x30);    /* 0x30 = ascii 0 */
+    }
+    else if (digit <= 0x0F)
+    {
+        sys_log_print_byte(digit + 0x37);    /* 0x37 = ascii 7 */
+    }
+    else
+    {
+        sys_log_print_byte('N');
+    }
+}
+
+void sys_log_print_str(char *str)
+{
+    uint16_t i = 0;
+    while(str[i] != '\0')
+    {
+        sys_log_print_byte(str[i]);
+        i++;
+    }
+}
+
+void sys_log_print_uint(uint32_t uint)
+{
+    if (uint == 0)
+    {
+        sys_log_print_digit(0);
+    }
+    else
+    {
+        uint8_t uint_str[10];               /* 32-bits = decimal with 10 digits */
+
+        uint8_t digits = log10(uint) + 1;
+
+        uint8_t i = 0;
+        for(i=0; i<digits; ++i, uint /= 10)
+        {
+            uint_str[i] = uint % 10;
+        }
+
+        uint8_t j = 0;
+        for(j=i; j>0; j--)
+        {
+            sys_log_print_digit(uint_str[j-1]);
+        }
+    }
+}
+
+void sys_log_print_int(int32_t sint)
+{
+    if (sint < 0)
+    {
+        sint = abs(sint);
+        sys_log_print_msg("-");
+    }
+
+    sys_log_print_uint((uint32_t)sint);
+}
+
+void sys_log_print_hex(uint32_t hex)
+{
+    sys_log_print_msg("0x");
+    
+    if (hex > 0x00FFFFFF)
+    {
+        sys_log_print_digit((uint8_t)(hex >> 28) & 0x0F);
+        sys_log_print_digit((uint8_t)(hex >> 24) & 0x0F);
+    }
+
+    if (hex > 0x0000FFFF)
+    {
+        sys_log_print_digit((uint8_t)(hex >> 20) & 0x0F);
+        sys_log_print_digit((uint8_t)(hex >> 16) & 0x0F);
+    }
+
+    if (hex > 0x000000FF)
+    {
+        sys_log_print_digit((uint8_t)(hex >> 12) & 0x0F);
+        sys_log_print_digit((uint8_t)(hex >> 8) & 0x0F);
+    }
+
+    sys_log_print_digit((uint8_t)(hex >> 4) & 0x0F);
+    sys_log_print_digit((uint8_t)(hex & 0x0F));
+}
+
+void sys_log_dump_hex(uint8_t *data, uint16_t len)
+{
+    uint16_t i = 0;
+    for(i=0; i<len; i++)
+    {
+        sys_log_print_hex(data[i]);
+
+        if (i < len-1)
+        {
+            sys_log_print_msg(", ");
+        }
+    }
+}
+
+void sys_log_print_float(float flt, uint8_t digits)
+{
+    if (flt < 0)
+    {
+        sys_log_print_msg("-");
+
+        flt = abs(flt);
+    }
+
+    /* Extract integer part */
+    uint32_t ipart = (uint32_t)flt;
+
+    /* Extract floating part */
+    float fpart = flt - (float)ipart;
+
+    /* Print integer part */
+    sys_log_print_uint(ipart);
+
+    /* Print decimal point */
+    sys_log_print_msg(".");
+
+    /* Print floating part */
+    sys_log_print_uint((uint32_t)(fpart*pow(10, digits)));
+}
+
+void sys_log_print_byte(uint8_t byte)
+{
+    sys_log_uart_write_byte(byte);
+}
+
+void sys_log_print_system_time()
+{
+    sys_log_set_color(SYS_LOG_SYSTEM_TIME_COLOR);
+
+    sys_log_print_msg("[ ");
+    sys_log_print_uint(xTaskGetTickCount());    /* System time in milliseconds */
+    sys_log_print_msg(" ]");
+
+    sys_log_reset_color();
+}
+
+void sys_log_print_license_msg()
+{
+    sys_log_print_msg("EPS 2.0 Copyright (C) 2020, SpaceLab;");
+    sys_log_new_line();
+    sys_log_print_msg("This program comes with ABSOLUTELY NO WARRANTY.");
+    sys_log_new_line();
+    sys_log_print_msg("This is free software, and you are welcome to redistribute it");
+    sys_log_new_line();
+    sys_log_print_msg("under certain conditions.");
+    sys_log_new_line();
+    sys_log_new_line();
+    sys_log_print_msg("Source code: https://github.com/spacelab-ufsc/obdh2");
+    sys_log_new_line();
+    sys_log_print_msg("Documentation: https://github.com/spacelab-ufsc/obdh2/doc");
+    sys_log_new_line();
+}
+
+void sys_log_print_splash_screen()
+{
+    sys_log_print_msg("                                                                   ");
+    sys_log_new_line();
+    sys_log_print_msg("                                                                   ");
+    sys_log_new_line();
+    sys_log_print_msg("...................................................................");
+    sys_log_new_line();
+    sys_log_print_msg("...................................................................");
+    sys_log_new_line();
+    sys_log_print_msg("..........                                               ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........  ____                       _          _      ..........");
+    sys_log_new_line();
+    sys_log_print_msg(".......... / ___| _ __   __ _  ___ ___| |    __ _| |__   ..........");
+    sys_log_new_line();
+    sys_log_print_msg(".......... \\___ \\| '_ \\ / _` |/ __/ _ \\ |   / _` | '_ \\  ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........  ___) | |_) | (_| | (_|  __/ |__| (_| | |_) | ..........");
+    sys_log_new_line();
+    sys_log_print_msg(".......... |____/| .__/ \\__,_|\\___\\___|_____\\__,_|_.__/  ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........       |_|                                     ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........                                               ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........        _____ ____  ____    ____    ___        ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........       | ____|  _ \\/ ___|  |___ \\  / _ \\       ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........       |  _| | |_) \\___ \\    __) || | | |      ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........       | |___|  __/ ___) |  / __/ | |_| |      ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........       |_____|_|   |____/  |_____(_)___/       ..........");
+    sys_log_new_line();
+    sys_log_new_line();
+    sys_log_print_msg("..........                                               ..........");
+    sys_log_new_line();
+    sys_log_print_msg("..........                                               ..........");
+    sys_log_new_line();
+    sys_log_print_msg("...................................................................");
+    sys_log_new_line();
+    sys_log_print_msg("...................................................................");
+    sys_log_new_line();
+    sys_log_print_msg("                                                                   ");
+    sys_log_new_line();
+    sys_log_print_msg("                                                                   ");
+    sys_log_new_line();
+}
+
+void sys_log_print_firmware_version()
+{
+    sys_log_print_msg("[ ");
+    sys_log_print_msg(FIRMWARE_VERSION);
+    sys_log_print_msg(" ]");
+}
+
+/** \} End of sys_log group */
---
+++ firmware/system/sys_log/sys_log.h
@@ -0,0 +1,321 @@
+/*
+ * sys_log.h
+ * 
+ * Copyright (C) 2020, SpaceLab.
+ * 
+ * This file is part of EPS 2.0.
+ * 
+ * EPS 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EPS 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with EPS 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Functions for printing messages and variables over an UART port.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 2020/10/22
+ * 
+ * \defgroup sys_log System Log
+ * \ingroup system
+ * \{
+ */
+
+#ifndef SYS_LOG_H_
+#define SYS_LOG_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <FreeRTOS.h>
+#include <semphr.h>
+
+/**
+ * \brief Event types.
+ */
+typedef enum
+{
+    SYS_LOG_INFO,               /**< Information message. */
+    SYS_LOG_WARNING,            /**< Warning message. */
+    SYS_LOG_ERROR               /**< Error message. */
+} sys_log_event_type_e;
+
+/**
+ * \brief System log text colors list.
+ */
+typedef enum
+{
+    SYS_LOG_COLOR_BLACK = 0,    /**< Color black. */
+    SYS_LOG_COLOR_RED,          /**< Color red. */
+    SYS_LOG_COLOR_GREEN,        /**< Color green. */
+    SYS_LOG_COLOR_YELLOW,       /**< Color yellow. */
+    SYS_LOG_COLOR_BLUE,         /**< Color blue. */
+    SYS_LOG_COLOR_MAGENTA,      /**< Color magenta. */
+    SYS_LOG_COLOR_CYAN,         /**< Color cyan. */
+    SYS_LOG_COLOR_WHITE         /**< Color white. */
+} sys_log_colors_e;
+
+/**
+ * \brief System log mutex.
+ */
+extern SemaphoreHandle_t xSysLogSemaphore;
+
+/**
+ * \brief Initialization of the system log.
+ * 
+ * \return The error/status code.
+ */
+int sys_log_init();
+
+/**
+ * \brief Sets the foreground color for the next log message.
+ *
+ * This function uses the ANSI color code to output color log messages.
+ *
+ * \param[in] color is the foreground color of text to select. It can be:
+ * \parblock
+ *      -\b SYS_LOG_COLOR_BLACK
+ *      -\b SYS_LOG_COLOR_RED
+ *      -\b SYS_LOG_COLOR_GREEN
+ *      -\b SYS_LOG_COLOR_YELLOW
+ *      -\b SYS_LOG_COLOR_BLUE
+ *      -\b SYS_LOG_COLOR_MAGENTA
+ *      -\b SYS_LOG_COLOR_CYAN
+ *      -\b SYS_LOG_COLOR_WHITE
+ *      .
+ * \endparblock
+ *
+ * \return None.
+ */
+void sys_log_set_color(uint8_t color);
+
+/**
+ * \brief Resets the color back to normal.
+ *
+ * \return None.
+ */
+void sys_log_reset_color();
+
+/**
+ * \brief Prints a general event.
+ *
+ * \param[in] type is the type of event. It can be:
+ * \parblock
+ *      -\b SYS_LOG_INFO
+ *      -\b SYS_LOG_WARNING
+ *      -\b SYS_LOG_ERROR
+ *      .
+ * \endparblock
+ *
+ * \param[in] event is the event text.
+ *
+ * \return None.
+ */
+void sys_log_print_event(uint8_t type, const char *event);
+
+/**
+ * \brief Prints an event from a system module.
+ *
+ * \param[in] type is the type of event. It can be:
+ * \parblock
+ *      -\b SYS_LOG_INFO
+ *      -\b SYS_LOG_WARNING
+ *      -\b SYS_LOG_ERROR
+ *      .
+ * \endparblock
+ *
+ * \param[in] module is the module name.
+ *
+ * \param[in] event is the event text.
+ *
+ * \return None.
+ */
+void sys_log_print_event_from_module(uint8_t type, const char *module, const char *event);
+
+/**
+ * \brief Prints a message over the system log module.
+ * 
+ * \param[in] msg is the message to be written.
+ * 
+ * \return None.
+ */
+void sys_log_print_msg(const char *msg);
+
+/**
+ * \brief Prints an string over the system log module.
+ *
+ * \param[in] str is a pointer to an string.
+ *
+ * \return None.
+ */
+void sys_log_print_str(char *str);
+
+/**
+ * \brief Prints a new line.
+ *
+ * \return None.
+ */
+void sys_log_new_line();
+
+/**
+ * \brief Prints a integer digit over the system log module.
+ * 
+ * \param[in] d is the digit to be written (0 to 9)
+ * 
+ * \return None.
+ */
+void sys_log_print_digit(uint8_t d);
+
+/**
+ * \brief Prints an unsigned integer over the system log module.
+ *
+ * \param[in] uint is the unsigned integer number to print.
+ *
+ * \return None.
+ */
+void sys_log_print_uint(uint32_t uint);
+
+/**
+ * \brief Prints a signed integer over the system log module.
+ *
+ * \param[in] sint is the signed integer number to print.
+ *
+ * \return None.
+ */
+void sys_log_print_int(int32_t sint);
+
+/**
+ * \brief Prints a hexadecimal value over the system log module.
+ * 
+ * Example:
+ *      - Integer   = 0x65
+ *      - Output    = "0x65"
+ * 
+ * \note The integer is printed in ASCII code.
+ * 
+ * \param hex is the hexadecimal to be written.
+ * 
+ * \return None.
+ */
+void sys_log_print_hex(uint32_t hex);
+
+/**
+ * \brief Prints a array of bytes in hexadecimal format.
+ *
+ * \param[in] data is an array of bytes to print.
+ *
+ * \param[in] len is the number of bytes to print.
+ *
+ * \return None.
+ */
+void sys_log_dump_hex(uint8_t *data, uint16_t len);
+
+/**
+ * \brief Prints a float number over the system log module.
+ *
+ * \param[in] flt is the float number to print.
+ *
+ * \param[in] digits is the number of digits to print.
+ *
+ * \return None.
+ */
+void sys_log_print_float(float flt, uint8_t digits);
+
+/**
+ * \brief Prints a raw byte over the system log module.
+ * 
+ * \param[in] byte is the byte to be printed.
+ * 
+ * \return None.
+ */
+void sys_log_print_byte(uint8_t byte);
+
+/**
+ * \brief Prints the system time in milliseconds.
+ *
+ * \return None.
+ */
+void sys_log_print_system_time();
+
+/**
+ * \brief Prints the license text and genreal firmware information.
+ *
+ * \return None.
+ */
+void sys_log_print_license_msg();
+
+/**
+ * \brief Prints the splash screen of the firmware.
+ *
+ * \return None.
+ */
+void sys_log_print_splash_screen();
+
+/**
+ * \brief Writes the current firmware version.
+ *
+ * \return None.
+ */
+void sys_log_print_firmware_version();
+
+/**
+ * \brief Initialization of the system log UART port.
+ * 
+ * UART settings:
+ *      -\b MCU interface = USCI_A1
+ *      -\b Baudrate      = 115200 bps
+ *      -\b Data bits     = 8
+ *      -\b Parity bit    = None
+ *      -\b Stop bits     = 1
+ *      .
+ * 
+ * \return TRUE/FALSE if successful or not.
+ */
+bool sys_log_uart_init();
+
+/**
+ * \brief Writes a byte over the UART port.
+ *
+ * \param[in] byte is the byte to be written.
+ *
+ * \return None.
+ */
+void sys_log_uart_write_byte(uint8_t byte);
+
+/**
+ * \brief Creates a mutex to use the system log module.
+ *
+ * \return TRUE/FALSE if successful or not.
+ */
+bool sys_log_mutex_create();
+
+/**
+ * \brief Holds the resource (system log module).
+ *
+ * \return TRUE/FALSE if successful or not.
+ */
+bool sys_log_mutex_take();
+
+/**
+ * \brief Frees the resource (system log).
+ *
+ * \return TRUE/FALSE if successful or not.
+ */
+bool sys_log_mutex_give();
+
+#endif /* SYS_LOG_H_ */
+
+/** \} End of sys_log group */
