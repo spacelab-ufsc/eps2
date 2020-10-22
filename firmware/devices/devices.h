b90d75194eada50cfbb372e62e07ccc6dd34a34e
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-10-21 21:28:22 -0300

    firmware: Importing the firmware structure from OBDH 2.0 #22

---
+++ firmware/devices/devices.h
@@ -0,0 +1,41 @@
+/*
+ * devices.h
+ * 
+ * Copyright (C) 2020, SpaceLab.
+ * 
+ * This file is part of EPS 2.0.
+ * 
+ * EPS 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EPS 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with EPS 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Devices layer.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 2020/10/21
+ * 
+ * \defgroup devices Devices
+ * \{
+ */
+
+#ifndef DEVICES_H_
+#define DEVICES_H_
+
+#endif /* DEVICES_H_ */
+
+/** \} End of devices group */
