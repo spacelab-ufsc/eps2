41194934c60fc0c969985b7e79a5d5700ebea7da
vinicius bernardo <viniciuspibi@gmail.com>
2021-01-17 11:25:59 -0300

    firmware: drivers: adc: changing adc ports initialized from obdh2 initial layout

--- firmware/drivers/adc/adc.c
+++ firmware/drivers/adc/adc.c
@@ -49,10 +49,10 @@ int adc_init(adc_port_t port, adc_config_t config)
 {
     if (adc_is_ready)
     {
-    #if CONFIG_DRIVERS_DEBUG_ENABLED == 1
+#if CONFIG_DRIVERS_DEBUG_ENABLED == 1
         sys_log_print_event_from_module(SYS_LOG_WARNING, ADC_MODULE_NAME, "ADC driver already initialized!");
         sys_log_new_line();
-    #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
+#endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
         return 0;
     }
 
@@ -65,14 +65,26 @@ int adc_init(adc_port_t port, adc_config_t config)
     /* Enable sample timer */
     ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1;
 
-    P6SEL |= (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
+    /* Set port 6 and 7 pins as inputs */
+    P6SEL |= (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+    P7SEL |= (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
 
-    ADC12MCTL0 = ADC12SREF_2 | ADC12INCH_0;                 /* Daughterboard ADC0. */
-    ADC12MCTL1 = ADC12SREF_2 | ADC12INCH_1;                 /* Daughterboard ADC1. */
-    ADC12MCTL2 = ADC12SREF_2 | ADC12INCH_2;                 /* Daughterboard ADC2. */
-    ADC12MCTL3 = ADC12SREF_2 | ADC12INCH_3;                 /* Current sensor. */
-    ADC12MCTL4 = ADC12SREF_2 | ADC12INCH_4;                 /* Voltage sensor. */
-    ADC12MCTL5 = ADC12EOS | ADC12SREF_2 | ADC12INCH_10;     /* Temperature sensor. */
+    ADC12MCTL0 = ADC12INCH_0 | ADC12SREF_2;              /* ADC1_I, ref = Vref+, channel = A0 */
+    ADC12MCTL1 = ADC12INCH_1 | ADC12SREF_2;              /* ADC2_I, ref = Vref+, channel = A1 */
+    ADC12MCTL2 = ADC12INCH_2 | ADC12SREF_2;              /* ADC3_I, ref = Vref+, channel = A2 */
+    ADC12MCTL3 = ADC12INCH_3 | ADC12SREF_2;              /* ADC4_I, ref = Vref+, channel = A3 */
+    ADC12MCTL4 = ADC12INCH_4 | ADC12SREF_2;              /* ADC5_I, ref = Vref+, channel = A4 */
+    ADC12MCTL5 = ADC12INCH_5 | ADC12SREF_2;              /* ADC6_I, ref = Vref+, channel = A5 */
+    ADC12MCTL6 = ADC12INCH_6 | ADC12SREF_2;              /* ADC7_I, ref = Vref+, channel = A6 */
+    ADC12MCTL7 = ADC12INCH_7 | ADC12SREF_2;              /* ADC_BUS, ref = Vref+, channel = A7 */
+    ADC12MCTL8 = ADC12INCH_8 | ADC12SREF_2;              /* ref = Vref+, channel = A8 */
+    ADC12MCTL9 = ADC12INCH_9 | ADC12SREF_2;              /* ref = Vref+, channel = A9 */
+    ADC12MCTL10 = ADC12INCH_10 | ADC12SREF_2;            /* ref = Vref+, channel = A10 */
+    ADC12MCTL11 = ADC12INCH_11 | ADC12SREF_2;            /* ref = Vref+, channel = A11 */
+    ADC12MCTL12 = ADC12INCH_12 | ADC12SREF_2;            /* ADC1_V, ref = Vref+, channel = A12 */
+    ADC12MCTL13 = ADC12INCH_13 | ADC12SREF_2;            /* ADC2_V, ref = Vref+, channel = A13 */
+    ADC12MCTL14 = ADC12INCH_14 | ADC12SREF_2;            /* ADC3_V, ref = Vref+, channel = A14 */
+    ADC12MCTL15 = ADC12INCH_15 | ADC12SREF_2 | ADC12EOS; /* ADC_TOTAL_V, ref = Vref+, channel = A15, End of Sequence flag */
 
     /* Allow ~100us (at default UCS settings) for REF to settle */
     adc_delay_ms(1);
@@ -86,113 +98,130 @@ int adc_init(adc_port_t port, adc_config_t config)
 
 int adc_read(adc_port_t port, uint16_t *val)
 {
-    while(ADC12CTL1 & ADC12BUSY);
+    while (ADC12CTL1 & ADC12BUSY)
+        ;
 
     ADC12CTL0 &= ~ADC12SC;
     ADC12CTL0 |= ADC12SC;
 
-    switch(port)
+    switch (port)
     {
-        case ADC_PORT_0:
-            while(!(ADC12IFG & (1 << 0)));
+    case ADC_PORT_0:
+        while (!(ADC12IFG & (1 << 0)))
+            ;
 
-            *val = ADC12MEM0;
+        *val = ADC12MEM0;
 
-            break;
-        case ADC_PORT_1:
-            while(!(ADC12IFG & (1 << 1)));
+        break;
+    case ADC_PORT_1:
+        while (!(ADC12IFG & (1 << 1)))
+            ;
 
-            *val = ADC12MEM1;
+        *val = ADC12MEM1;
 
-            break;
-        case ADC_PORT_2:
-            while(!(ADC12IFG & (1 << 2)));
+        break;
+    case ADC_PORT_2:
+        while (!(ADC12IFG & (1 << 2)))
+            ;
 
-            *val = ADC12MEM2;
+        *val = ADC12MEM2;
 
-            break;
-        case ADC_PORT_3:
-            while(!(ADC12IFG & (1 << 3)));
+        break;
+    case ADC_PORT_3:
+        while (!(ADC12IFG & (1 << 3)))
+            ;
 
-            *val = ADC12MEM3;
+        *val = ADC12MEM3;
 
-            break;
-        case ADC_PORT_4:
-            while(!(ADC12IFG & (1 << 4)));
+        break;
+    case ADC_PORT_4:
+        while (!(ADC12IFG & (1 << 4)))
+            ;
 
-            *val = ADC12MEM4;
+        *val = ADC12MEM4;
 
-            break;
-        case ADC_PORT_5:
-            while(!(ADC12IFG & (1 << 5)));
+        break;
+    case ADC_PORT_5:
+        while (!(ADC12IFG & (1 << 5)))
+            ;
 
-            *val = ADC12MEM5;
+        *val = ADC12MEM5;
 
-            break;
-        case ADC_PORT_6:
-            while(!(ADC12IFG & (1 << 6)));
+        break;
+    case ADC_PORT_6:
+        while (!(ADC12IFG & (1 << 6)))
+            ;
 
-            *val = ADC12MEM6;
+        *val = ADC12MEM6;
 
-            break;
-        case ADC_PORT_7:
-            while(!(ADC12IFG & (1 << 7)));
+        break;
+    case ADC_PORT_7:
+        while (!(ADC12IFG & (1 << 7)))
+            ;
 
-            *val = ADC12MEM7;
+        *val = ADC12MEM7;
 
-            break;
-        case ADC_PORT_8:
-            while(!(ADC12IFG & (1 << 8)));
+        break;
+    case ADC_PORT_8:
+        while (!(ADC12IFG & (1 << 8)))
+            ;
 
-            *val = ADC12MEM8;
+        *val = ADC12MEM8;
 
-            break;
-        case ADC_PORT_9:
-            while(!(ADC12IFG & (1 << 9)));
+        break;
+    case ADC_PORT_9:
+        while (!(ADC12IFG & (1 << 9)))
+            ;
 
-            *val = ADC12MEM9;
+        *val = ADC12MEM9;
 
-            break;
-        case ADC_PORT_10:
-            while(!(ADC12IFG & (1 << 10)));
+        break;
+    case ADC_PORT_10:
+        while (!(ADC12IFG & (1 << 10)))
+            ;
 
-            *val = ADC12MEM10;
+        *val = ADC12MEM10;
 
-            break;
-        case ADC_PORT_11:
-            while(!(ADC12IFG & (1 << 11)));
+        break;
+    case ADC_PORT_11:
+        while (!(ADC12IFG & (1 << 11)))
+            ;
 
-            *val = ADC12MEM11;
+        *val = ADC12MEM11;
 
-            break;
-        case ADC_PORT_12:
-            while(!(ADC12IFG & (1 << 12)));
+        break;
+    case ADC_PORT_12:
+        while (!(ADC12IFG & (1 << 12)))
+            ;
 
-            *val = ADC12MEM12;
+        *val = ADC12MEM12;
 
-            break;
-        case ADC_PORT_13:
-            while(!(ADC12IFG & (1 << 13)));
+        break;
+    case ADC_PORT_13:
+        while (!(ADC12IFG & (1 << 13)))
+            ;
 
-            *val = ADC12MEM13;
+        *val = ADC12MEM13;
 
-            break;
-        case ADC_PORT_14:
-            while(!(ADC12IFG & (1 << 14)));
+        break;
+    case ADC_PORT_14:
+        while (!(ADC12IFG & (1 << 14)))
+            ;
 
-            *val = ADC12MEM14;
+        *val = ADC12MEM14;
 
-            break;
-        case ADC_PORT_15:
-            while(!(ADC12IFG & (1 << 15)));
+        break;
+    case ADC_PORT_15:
+        while (!(ADC12IFG & (1 << 15)))
+            ;
 
-            *val = ADC12MEM15;
+        *val = ADC12MEM15;
 
-            break;
-        default:
-            *val = UINT16_MAX;
+        break;
+    default:
+        *val = UINT16_MAX;
 
-            return -1;
+        return -1;
     }
 
     return 0;
