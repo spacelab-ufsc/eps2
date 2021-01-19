8eb8ee8925ffa0c20dc086ea332cc4ebc972a1d8
Yan Azeredo <yan.ufsceel@gmail.com>
2021-01-18 22:33:00 -0300

    firmware: drivers: ads1248: Adding ADS1248_RESET status, ads1248_delay function, updating ads1248_init and first version of the reset function #29

--- firmware/drivers/ads1248/ads1248.c
+++ firmware/drivers/ads1248/ads1248.c
@@ -25,7 +25,7 @@
  * 
  * \authors Gabriel Mariano Marcelino <gabriel.mm8@gmail.com> and Yan Castro de Azeredo <yan.ufsceel@gmail.com>
  * 
- * \version 0.1.2
+ * \version 0.1.3
  * 
  * \date 2020/10/24
  * 
@@ -38,8 +38,20 @@
 
 #include "ads1248.h"
 
+void ads1248_delay(uint8_t ms)
+{
+    vTaskDelay(pdMS_TO_TICKS(ms));
+}
+
 int ads1248_init(ads1248_config_t *config)
 {
+    ads1248_delay(16); // 16 ms delay to allow power supplies to settle and power-on reset to complete
+    
+    #if CONFIG_DRIVERS_DEBUG_ENABLED == 1
+        sys_log_print_event_from_module(SYS_LOG_INFO, ADS1248_MODULE_NAME, "Initializing the ADS1248...");
+        sys_log_new_line();
+    #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
+
     int res_start = gpio_init(config->start_pin, (gpio_config_t){.mode=GPIO_MODE_OUTPUT});
     
     int res_reset = gpio_init(config->reset_pin, (gpio_config_t){.mode=GPIO_MODE_OUTPUT});
@@ -64,15 +76,6 @@ int ads1248_init(ads1248_config_t *config)
         return ADS1248_ERROR;
     }
 
-    if (gpio_set_state(config->reset_pin, true) != 0)
-    {
-    #if CONFIG_DRIVERS_DEBUG_ENABLED == 1
-        sys_log_print_event_from_module(SYS_LOG_ERROR, ADS1248_MODULE_NAME, "Error during setting RESET pin to high state during initialization!");
-        sys_log_new_line();
-    #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
-        return ADS1248_ERROR;
-    }
-
     if (spi_select_slave(config->spi_port, config->spi_cs, false) != 0)
     {
     #if CONFIG_DRIVERS_DEBUG_ENABLED == 1
@@ -81,12 +84,43 @@ int ads1248_init(ads1248_config_t *config)
     #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
         return ADS1248_ERROR;
     }
+    
+    // Delay time (10ns) for first SCLK rising edge after CS falling edge needed?
+    
+    ads1248_write_cmd(config, ADS1248_RESET_PIN); // Sends reset command to make sure the device is properly reset after power up;
+
+    ads1248_delay(1); // 1 ms delay time for SCLK rising edge (start of serial interface communication) after RESET rising edge
+
+    ads1248_write_cmd(config, ADS1248_CMD_SDATAC); // Sends stop read data continuous mode command;
+
     return ADS1248_READY;
 }
 
 int ads1248_reset(ads1248_config_t *config, ads1248_reset_mode_t mode)
 {
-    return -1;
+    if (mode != 0) // if the reset mode is by command (ADS1248_RESET_CMD = 1)
+    {
+        if (ads1248_write_cmd(config, ADS1248_CMD_RESET) != 0);
+            {
+            #if CONFIG_DRIVERS_DEBUG_ENABLED == 1
+                sys_log_print_event_from_module(SYS_LOG_ERROR, ADS1248_MODULE_NAME, "Error sending RESET command!");
+                sys_log_new_line();
+            #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
+                return ADS1248_ERROR;
+            }
+    }
+    else        // if the reset mode is using the rest pin (ADS1248_RESET_PIN = 0)
+    {
+        if (gpio_set_state(config->reset_pin, true) != 0)
+            {
+            #if CONFIG_DRIVERS_DEBUG_ENABLED == 1
+                sys_log_print_event_from_module(SYS_LOG_ERROR, ADS1248_MODULE_NAME, "Error during setting RESET pin to high state!");
+                sys_log_new_line();
+            #endif /* CONFIG_DRIVERS_DEBUG_ENABLED */
+                return ADS1248_ERROR;
+            }
+    }
+    return ADS1248_RESET;
 }
 
 int ads1248_set_powerdown_mode(ads1248_config_t *config)
