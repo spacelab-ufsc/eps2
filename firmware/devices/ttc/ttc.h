8e8f379845ea6e1491440d73c51a580082f8913b
Augusto <augustovassoler@gmail.com>
2021-04-22 15:55:15 -0300

    firmware: devices: ttc: adding ttc device initial structure #76

---
+++ firmware/devices/ttc/ttc.h
@@ -0,0 +1,69 @@
+/*
+ * ttc.h
+ *
+ * Copyright (C) 2020, SpaceLab.
+ *
+ * This file is part of OBDH 2.0.
+ *
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \brief TTC device definition.
+ *
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ *
+ * \version 0.2.0
+ *
+ * \date 01/02/2020
+ *
+ * \defgroup ttc TTC
+ * \ingroup devices
+ * \{
+ */
+
+#ifndef TTC_H_
+#define TTC_H_
+
+#include <stdint.h>
+
+#define TTC_MODULE_NAME         "TTC"
+
+/**
+ * \brief Initialization routine of the TTC device.
+ *
+ * \return The status/error code.
+ */
+int ttc_init();
+
+/**
+ * \brief Enables the TTC hibernation for a given period.
+ *
+ * \param[in] period_min is the period of hibernation in minutes.
+ *
+ * \return The status/error code.
+ */
+int ttc_enter_hibernation(uint32_t period_min);
+
+/**
+ * \brief Disables the TTC hibernation.
+ *
+ * \return The status/error code.
+ */
+int ttc_leave_hibernation();
+
+#endif /* TTC_H_ */
+
+/** \} End of ttc group */
