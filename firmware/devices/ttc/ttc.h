bd8c5984a8e36d08eec855a6c5d2df5ecd807ff8
Augusto <augustovassoler@gmail.com>
2021-04-22 16:15:09 -0300

    firmware: devices: ttc: correcting header informations #76

--- firmware/devices/ttc/ttc.h
+++ firmware/devices/ttc/ttc.h
@@ -3,31 +3,31 @@
  *
  * Copyright (C) 2020, SpaceLab.
  *
- * This file is part of OBDH 2.0.
+ * This file is part of EPS 2.0.
  *
- * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * EPS 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OBDH 2.0 is distributed in the hope that it will be useful,
+ * EPS 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * along with EPS 2.0. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
 /**
  * \brief TTC device definition.
  *
- * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * \author Augusto Cezar Boldori Vassoler <augustovassoler@gmail.com>
  *
- * \version 0.2.0
+ * \version 0.1.0
  *
- * \date 01/02/2020
+ * \date 22/04/2020
  *
  * \defgroup ttc TTC
  * \ingroup devices
