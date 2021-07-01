aa0c42a025422a11b8c3feb19b50e0081346f200
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-07-01 00:57:00 -0300

    firmware: app: tasks: param_server: Task implementation #136

--- firmware/app/tasks/param_server.c
+++ firmware/app/tasks/param_server.c
@@ -25,13 +25,16 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.2.1
+ * \version 0.2.2
  * 
  * \date 2021/06/30
  * 
  * \addtogroup param_server
  * \{
  */
+
+#include <system/sys_log/sys_log.h>
+#include <structs/eps2_data.h>
 
 #include "param_server.h"
 
@@ -46,7 +49,112 @@ void vTaskParamServer(void *pvParameters)
     {
         TickType_t last_cycle = xTaskGetTickCount();
 
-        /* TODO */
+        eps2_param_id_t req_id = UINT8_MAX;
+
+        switch(req_id)
+        {
+            case EPS2_PARAM_ID_TIME_COUNTER:
+                break;
+            case EPS2_PARAM_ID_MCU_TEMP:
+                break;
+            case EPS2_PARAM_ID_EPS_CURRENT:
+                break;
+            case EPS2_PARAM_ID_LAST_RESET_CAUSE:
+                break;
+            case EPS2_PARAM_ID_RESET_COUNTER:
+                break;
+            case EPS2_PARAM_ID_SP_MY_PX_VOLTAGE:
+                break;
+            case EPS2_PARAM_ID_SP_MX_PZ_VOLTAGE:
+                break;
+            case EPS2_PARAM_ID_SP_MZ_PY_VOLTAGE:
+                break;
+            case EPS2_PARAM_ID_SP_MY_CURRENT:
+                break;
+            case EPS2_PARAM_ID_SP_PY_CURRENT:
+                break;
+            case EPS2_PARAM_ID_SP_MX_CURRENT:
+                break;
+            case EPS2_PARAM_ID_SP_PX_CURRENT:
+                break;
+            case EPS2_PARAM_ID_SP_MZ_CURRENT:
+                break;
+            case EPS2_PARAM_ID_SP_PZ_CURRENT:
+                break;
+            case EPS2_PARAM_ID_MPPT_1_DUTY_CYCLE:
+                break;
+            case EPS2_PARAM_ID_MPPT_2_DUTY_CYCLE:
+                break;
+            case EPS2_PARAM_ID_MPPT_3_DUTY_CYCLE:
+                break;
+            case EPS2_PARAM_ID_SP_VOLTAGE_MPPT:
+                break;
+            case EPS2_PARAM_ID_MAIN_POWER_BUS_VOLTAGE:
+                break;
+            case EPS2_PARAM_ID_RTD_0_TEMP:
+                break;
+            case EPS2_PARAM_ID_RTD_1_TEMP:
+                break;
+            case EPS2_PARAM_ID_RTD_2_TEMP:
+                break;
+            case EPS2_PARAM_ID_RTD_3_TEMP:
+                break;
+            case EPS2_PARAM_ID_RTD_4_TEMP:
+                break;
+            case EPS2_PARAM_ID_RTD_5_TEMP:
+                break;
+            case EPS2_PARAM_ID_RTD_6_TEMP:
+                break;
+            case EPS2_PARAM_ID_BAT_VOLTAGE:
+                break;
+            case EPS2_PARAM_ID_BAT_CURRENT:
+                break;
+            case EPS2_PARAM_ID_BAT_AVERAGE_CURRENT:
+                break;
+            case EPS2_PARAM_ID_BAT_ACC_CURRENT:
+                break;
+            case EPS2_PARAM_ID_BAT_CHARGE:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_TEMP:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_STATUS:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_PROTECT:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_CYCLE_COUNTER:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_RAAC:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_RSAC:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_RARC:
+                break;
+            case EPS2_PARAM_ID_BAT_MONITOR_RSRC:
+                break;
+            case EPS2_PARAM_ID_BAT_HEATER_1_DUTY_CYCLE:
+                break;
+            case EPS2_PARAM_ID_BAT_HEATER_2_DUTY_CYCLE:
+                break;
+            case EPS2_PARAM_ID_HW_VERSION:
+                break;
+            case EPS2_PARAM_ID_FW_VERSION:
+                break;
+            case EPS2_PARAM_ID_MPPT_1_MODE:
+                break;
+            case EPS2_PARAM_ID_MPPT_2_MODE:
+                break;
+            case EPS2_PARAM_ID_MPPT_3_MODE:
+                break;
+            case EPS2_PARAM_ID_BAT_HEATER_1_MODE:
+                break;
+            case EPS2_PARAM_ID_BAT_HEATER_2_MODE:
+                break;
+            case EPS2_PARAM_ID_DEVICE_ID:
+                break;
+            default:
+                sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_PARAM_SERVER_NAME, "Unknown parameter ID!");
+                sys_log_new_line();
+        }
 
         vTaskDelayUntil(&last_cycle, pdMS_TO_TICKS(TASK_PARAM_SERVER_PERIOD_MS));
     }
