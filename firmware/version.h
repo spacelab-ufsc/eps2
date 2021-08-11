a561bfbe165bfd2e417f1463d972d2f998fa027b
Andre <andrempmattos@gmail.com>
2021-08-11 18:01:11 -0300

    firmware: app: structs: Fixing variable assignments #86

--- firmware/version.h
+++ firmware/version.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.2.16
+ * \version 0.2.17
  * 
  * \date 2020/10/21
  * 
@@ -36,7 +36,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define FIRMWARE_VERSION            "0.2.16"
+#define FIRMWARE_VERSION            "0.2.17"
 
 #define FIRMWARE_STATUS             "Development"
 
