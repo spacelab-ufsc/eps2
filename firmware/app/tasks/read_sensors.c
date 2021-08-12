53602cd86ebe3c0b629cb2cae64eaeb2e29bbcfd
Andre <andrempmattos@gmail.com>
2021-08-12 09:39:05 -0300

    firmware: app: tasks: read_sensors: Adding RTD sensor reads and typecasts #89

--- firmware/app/tasks/read_sensors.c
+++ firmware/app/tasks/read_sensors.c
@@ -26,7 +26,7 @@
  * \author Yan Castro de Azeredo <yan.ufsceel@gmail.com>
  * \author André M. P. de Mattos <andre.mattos@spacelab.ufsc.br>
  * 
- * \version 0.2.5
+ * \version 0.2.27
  * 
  * \date 2021/04/23
  * 
@@ -59,77 +59,120 @@ void vTaskReadSensors(void *pvParameters)
         /* -Y Solar Panel current in mA.*/
         if (current_sensor_read(PANNEL_MINUS_Y_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_MY_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_MY_CURRENT, (uint32_t *)&buf);
         }
 
         /* +X Solar Panel current in mA.*/
         if (current_sensor_read(PANNEL_PLUS_X_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_PX_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_PX_CURRENT, (uint32_t *)&buf);
         }
 
         /* -X Solar Panel current in mA.*/
         if (current_sensor_read(PANNEL_MINUS_X_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_MX_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_MX_CURRENT, (uint32_t *)&buf);
         }
         
         /* +Z Solar Panel current in mA.*/
         if (current_sensor_read(PANNEL_PLUS_Z_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_PZ_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_PZ_CURRENT, (uint32_t *)&buf);
         }
 
         /* -Z Solar Panel current in mA.*/
         if (current_sensor_read(PANNEL_MINUS_Z_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_MZ_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_MZ_CURRENT, (uint32_t *)&buf);
         }
 
         /* +Y Solar Panel current in mA.*/
         if (current_sensor_read(PANNEL_PLUS_Y_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_PY_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_PY_CURRENT, (uint32_t *)&buf);
         }
         
         /* -Y and +X Solar Panels voltage in mV.*/
         if (voltage_sensor_read(PANNELS_MINUS_Y_PLUS_X_VOLTAGE_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_MY_PX_VOLTAGE, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_MY_PX_VOLTAGE, (uint32_t *)&buf);
         }
         
         /* -Z and +Y Solar Panels voltage in mV.*/
         if (voltage_sensor_read(PANNELS_MINUS_X_PLUS_Z_VOLTAGE_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_MX_PZ_VOLTAGE, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_MX_PZ_VOLTAGE, (uint32_t *)&buf);
         }
 
         /* -Y and +X Solar Panels voltage in mV.*/
         if (voltage_sensor_read(PANNELS_MINUS_Z_PLUS_Y_VOLTAGE_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_MZ_PY_VOLTAGE, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_MZ_PY_VOLTAGE, (uint32_t *)&buf);
         }
 
         /* Total solar panels output voltage after MPPT in mV.*/
         if (voltage_sensor_read(TOTAL_SOLAR_PANNELS_VOLTAGE_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_SP_VOLTAGE_MPPT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_SP_VOLTAGE_MPPT, (uint32_t *)&buf);
         }
         
         /* Main power bus voltage in mV.*/
         if (voltage_sensor_read(MAIN_POWER_BUS_VOLTAGE_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_MAIN_POWER_BUS_VOLTAGE, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_MAIN_POWER_BUS_VOLTAGE, (uint32_t *)&buf);
         }
 
         /* EPS and TTC circuitry current in mA.*/
         if (current_sensor_read(EPS_BEACON_CURRENT_SENSOR_ADC_PORT, &buf) == 0)
         {
-            eps_buffer_write(EPS2_PARAM_ID_EPS_CURRENT, &buf);
+            eps_buffer_write(EPS2_PARAM_ID_EPS_CURRENT, (uint32_t *)&buf);
+        }
+
+        /* RTD 0 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_0, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_0_TEMP, (uint32_t *)&buf);
+        }
+
+        /* RTD 1 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_1, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_1_TEMP, (uint32_t *)&buf);
+        }
+
+        /* RTD 2 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_2, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_2_TEMP, (uint32_t *)&buf);
+        }
+
+        /* RTD 3 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_3, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_3_TEMP, (uint32_t *)&buf);
+        }
+
+        /* RTD 4 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_4, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_4_TEMP, (uint32_t *)&buf);
+        }
+
+        /* RTD 5 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_5, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_5_TEMP, (uint32_t *)&buf);
+        }
+
+        /* RTD 6 temperature .*/
+        if (temp_rtd_read_k(TEMP_SENSOR_RTD_CH_6, &buf) == 0)
+        {
+            eps_buffer_write(EPS2_PARAM_ID_RTD_6_TEMP, (uint32_t *)&buf);
         }
 
         /* Data timestamp */
-        eps_buffer_write(EPS2_PARAM_ID_TIME_COUNTER, (uint32_t)xTaskGetTickCount());
+        uint32_t time_counter = (uint32_t)xTaskGetTickCount();
+        eps_buffer_write(EPS2_PARAM_ID_TIME_COUNTER, &time_counter);
 
         vTaskDelayUntil(&last_cycle, pdMS_TO_TICKS(TASK_READ_SENSORS_PERIOD_MS));
     }
