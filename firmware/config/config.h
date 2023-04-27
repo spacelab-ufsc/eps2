16510f81da6dd3a2925d235e4786481f7666f500
joaoclaudioeb <joaoclaudiobarcellos@gmail.com>
2023-04-27 11:03:19 -0300

    firmware: Minor changes for upcoming release

--- firmware/config/config.h
+++ firmware/config/config.h
@@ -50,13 +50,13 @@
 #define CONFIG_TASK_SYSTEM_RESET_ENABLED				1
 #define CONFIG_TASK_READ_SENSORS_ENABLED                1
 #define CONFIG_TASK_PARAM_SERVER_ENABLED                1
-#define CONFIG_TASK_MPPT_ALGORITHM_ENABLED              0
+#define CONFIG_TASK_MPPT_ALGORITHM_ENABLED              1
 #define CONFIG_TASK_HEATER_CONTROLLER_ENABLED           1
 #define CONFIG_TASK_TIME_CONTROL_ENABLED                1
 #define CONFIG_TASK_DEVICE_RESPONSE_ENABLED             1
 
 /* Devices */
-#define CONFIG_SET_DUMMY_EPS                            1
+#define CONFIG_SET_DUMMY_EPS                            0
 #define CONFIG_DEV_LEDS_ENABLED                         1
 #define CONFIG_DEV_HEATER_ENABLED                       1
 #define CONFIG_DEV_MPPT_ENABLED                         1
