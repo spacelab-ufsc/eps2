d05b30373e61401d36d503091132f979346c6af7
Ramon Borba <ramonborba97@gmail.com>
2023-06-23 15:35:44 -0300

    firmware: devices: mppt: Modified mppt_init and mppt_set_duty_cycle to not use global variables #193

--- firmware/devices/mppt/mppt.c
+++ firmware/devices/mppt/mppt.c
@@ -86,24 +86,23 @@ int mppt_init(void)
     sys_log_new_line();
 
     /* Initialize the PWM parameters */
-    mppt_config.period_us = MPPT_PERIOD_INIT;
-    mppt_config.duty_cycle = MPPT_DUTY_CYCLE_INIT;
+    mppt_config_t config = { .period_us = MPPT_PERIOD_INIT, .duty_cycle = MPPT_DUTY_CYCLE_INIT };
 
-    if(pwm_init(MPPT_CONTROL_LOOP_CH_SOURCE, MPPT_CONTROL_LOOP_CH_0, mppt_config) != 0) 
+    if(pwm_init(MPPT_CONTROL_LOOP_CH_SOURCE, MPPT_CONTROL_LOOP_CH_0, config) != 0) 
     {
         sys_log_print_event_from_module(SYS_LOG_ERROR, MPPT_MODULE_NAME, "Error during the initialization (CH0)!");
         sys_log_new_line();
         return -1;
     }
 
-    if(pwm_init(MPPT_CONTROL_LOOP_CH_SOURCE, MPPT_CONTROL_LOOP_CH_1, mppt_config) != 0)
+    if(pwm_init(MPPT_CONTROL_LOOP_CH_SOURCE, MPPT_CONTROL_LOOP_CH_1, config) != 0)
     {
         sys_log_print_event_from_module(SYS_LOG_ERROR, MPPT_MODULE_NAME, "Error during the initialization (CH1)!");
         sys_log_new_line();
         return -1;
     }
 
-    if(pwm_init(MPPT_CONTROL_LOOP_CH_SOURCE, MPPT_CONTROL_LOOP_CH_2, mppt_config) != 0)
+    if(pwm_init(MPPT_CONTROL_LOOP_CH_SOURCE, MPPT_CONTROL_LOOP_CH_2, config) != 0)
     {
         sys_log_print_event_from_module(SYS_LOG_ERROR, MPPT_MODULE_NAME, "Error during the initialization (CH2)!");
         sys_log_new_line();
@@ -265,8 +264,8 @@ int decrease_duty_cycle(mppt_channel_t channel)
 
 int mppt_set_duty_cycle(mppt_channel_t channel, uint32_t duty_cycle)
 {
-    mppt_config.duty_cycle = duty_cycle;
-    return pwm_update(MPPT_CONTROL_LOOP_CH_SOURCE, channel, mppt_config);
+    mppt_config_t config = { .period_us = MPPT_PERIOD_INIT, .duty_cycle = duty_cycle };
+    return pwm_update(MPPT_CONTROL_LOOP_CH_SOURCE, channel, config);
 }
 
 
