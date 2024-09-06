42a2753c4f460ce77b6f6bd982f3315a3f7615a4
Ramon de Araujo Borba <ramonborba97@gmail.com>
2024-09-06 14:47:25 -0300

    firmware: devices: media: Import media device from ttc2

--- firmware/devices/media/media.c
+++ firmware/devices/media/media.c
@@ -1,101 +1,160 @@
 /*
  * media.c
- *
- * Copyright (C) 2020, SpaceLab.
- *
+ * 
+ * Copyright The EPS 2.0 Contributors.
+ * 
  * This file is part of EPS 2.0.
- *
+ * 
  * EPS 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * EPS 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with EPS 2.0. If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with EPS 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
+ * 
  */
 
 /**
  * \brief Media device implementation.
- *
- * \author Augusto Cezar Boldori Vassoler <augustovassoler@gmail.com>
- *
- * \version 0.1.0
- *
- * \date 02/15/2021
- *
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.19
+ * 
+ * \date 2020/07/21
+ * 
  * \addtogroup media
  * \{
  */
 
+#include <config/config.h>
 #include <system/sys_log/sys_log.h>
 
 #include <drivers/flash/flash.h>
 
 #include "media.h"
 
-int media_init()
+int media_init(media_t med)
 {
+    int err = -1;
 
-    sys_log_print_event_from_module(SYS_LOG_INFO, MEDIA_MODULE_NAME, "Initializing Media device.");
-    sys_log_new_line();
-    
-    int err = flash_init();
-
-    if (err != 0)
+    switch(med)
     {
-        sys_log_print_event_from_module(SYS_LOG_ERROR, MEDIA_MODULE_NAME, "Error initializing Media device!");
-        sys_log_new_line();
+        case MEDIA_INT_FLASH:
+            err = flash_init();
+
+            break;
+        default:
+            sys_log_print_event_from_module(SYS_LOG_ERROR, MEDIA_MODULE_NAME, "Invalid storage media to initialize!");
+            sys_log_new_line();
+
+            break;
     }
-    
 
     return err;
-
 }
 
-int media_write(uint32_t adr, uint32_t *data, uint16_t len)
+int media_write(media_t med, uint32_t adr, uint8_t *data, uint16_t len)
 {
-    /* Address index */
-    adr += FLASH_SEG_A_ADR;
+    int err = -1;
 
-    uint16_t i = 0;
-    for(i=0; i<len; i+=4)
+    switch(med)
     {
-    flash_write_long(data[i], (uint32_t*)(adr + i));
+        case MEDIA_INT_FLASH:
+        {
+            /* Address index */
+            uintptr_t adr_idx = adr + FLASH_SEG_A_ADR;
+
+            uint16_t i = 0;
+            for(i=0; i<len; ++i)
+            {
+                uintptr_t adr_counter = adr_idx + i;
+
+                flash_write_single(data[i], adr_counter);
+            }
+
+            err = 0;
+
+            break;
+        }
+        default:
+            sys_log_print_event_from_module(SYS_LOG_ERROR, MEDIA_MODULE_NAME, "Invalid storage media to write!");
+            sys_log_new_line();
+
+            break;
     }
 
-    return 0;
-
+    return err;
 }
 
-int media_read(uint32_t adr, uint32_t *data, uint16_t len)
+int media_read(media_t med, uint32_t adr, uint8_t *data, uint16_t len)
 {
-
-    /* Address index */
-    adr += FLASH_SEG_A_ADR;
+    int err = -1;
 
     uint16_t i = 0;
-    for(i=0; i<len; i+=4)
+
+    switch(med)
     {
-        data[i] = flash_read_long((uint32_t*)(adr + i));
+        case MEDIA_INT_FLASH:
+        {
+            /* Address index */
+            uintptr_t adr_idx = adr + FLASH_SEG_A_ADR;
+
+            for(i=0; i<len; ++i)
+            {
+                uintptr_t adr_counter = adr_idx + i;
+
+                data[i] = flash_read_single(adr_counter);
+            }
+
+            err = 0;
+
+            break;
+        }
+        default:
+            sys_log_print_event_from_module(SYS_LOG_ERROR, MEDIA_MODULE_NAME, "Invalid storage media to read!");
+            sys_log_new_line();
+
+            break;
     }
 
-    return 0;
-
+    return err;
 }
 
-int media_erase(uint32_t adr)
+int media_erase(media_t med, uint32_t sector)
 {
+    int err = -1;
 
-    flash_write_single(0xFF, (uint8_t*)adr);
+    switch(med)
+    {
+        case MEDIA_INT_FLASH:
+        {
+            if ((sector == FLASH_SEG_A_ADR) || (sector == FLASH_SEG_B_ADR))
+            {
+                flash_erase((uintptr_t)sector);
+                err = 0;
+            }
+            else
+            {
+                sys_log_print_event_from_module(SYS_LOG_ERROR, MEDIA_MODULE_NAME, "Erasing invalid sector!");
+                sys_log_new_line();
+            }
+            break;
+    }
+        default:
+            sys_log_print_event_from_module(SYS_LOG_ERROR, MEDIA_MODULE_NAME, "Invalid storage media to erase!");
+            sys_log_new_line();
 
-    return 0;
+            break;
+    }
 
+    return err;
 }
 
 /** \} End of media group */
--- firmware/devices/media/media.h
+++ firmware/devices/media/media.h
@@ -1,34 +1,34 @@
 /*
  * media.h
- *
- * Copyright (C) 2020, SpaceLab.
- *
+ * 
+ * Copyright The EPS 2.0 Contributors.
+ * 
  * This file is part of EPS 2.0.
- *
+ * 
  * EPS 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * EPS 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with EPS 2.0. If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
  */
 
 /**
  * \brief Media device definition.
- *
- * \author Augusto Cezar Boldori Vassoler <augustovassoler@gmail.com>
- *
- * \version 0.1.0
- *
- * \date 02/15/2021
- *
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.19
+ * 
+ * \date 2020/04/21
+ * 
  * \defgroup media Media
  * \ingroup devices
  * \{
@@ -39,6 +39,8 @@
 
 #include <stdint.h>
 
+#include <drivers/flash/flash.h>
+
 #define MEDIA_MODULE_NAME           "Media"
 
 /**
@@ -47,23 +49,39 @@
 typedef enum
 {
     MEDIA_INT_FLASH=0,      /**< Internal flash memory. */
-    MEDIA_NOR               /**< NOR flash memory. */
-} media_types_e;
+} media_t;
 
 /**
- * \brief Media type.
+ * \brief Erase operation types.
  */
-typedef uint8_t media_t;
+typedef enum
+{
+    MEDIA_ERASE_DIE=0,      /**< Erases the whole die. */
+    MEDIA_ERASE_SECTOR,     /**< Erases a sector. */
+    MEDIA_ERASE_SUB_SECTOR  /**< Erases a sub-sector. */
+} media_erase_t;
 
 /**
  * \brief Media initialization.
  *
+ * \param[in] med is the storage media to initiailize. It can be:
+ * \parblock
+ *      -\b MEDIA_INT_FLASH
+ *      .
+ * \endparblock
+ *
  * \return The status/error code.
  */
-int media_init();
+int media_init(media_t med);
 
 /**
  * \brief Writes data into a given address of a media device.
+ *
+ * \param[in] med is the storage media to write. It can be:
+ * \parblock
+ *      -\b MEDIA_INT_FLASH
+ *      .
+ * \endparblock
  *
  * \param[in] adr is the address to write data.
  *
@@ -73,10 +91,16 @@ int media_init();
  *
  * \return The status/error code.
  */
-int media_write(uint32_t adr, uint32_t *data, uint16_t len);
+int media_write(media_t med, uint32_t adr, uint8_t *data, uint16_t len);
 
 /**
  * \brief Reads data from a given address of a media device.
+ *
+ * \param[in] med is the storage media to read. It can be:
+ * \parblock
+ *      -\b MEDIA_INT_FLASH
+ *      .
+ * \endparblock
  *
  * \param[in] adr is the address to read.
  *
@@ -86,16 +110,30 @@ int media_write(uint32_t adr, uint32_t *data, uint16_t len);
  *
  * \return The status/error code.
  */
-int media_read(uint32_t adr, uint32_t *data, uint16_t len);
+int media_read(media_t med, uint32_t adr, uint8_t *data, uint16_t len);
 
 /**
  * \brief Erases a memory region from a media device.
  *
- * \param[in] adr is the address to erase.
+ * \param[in] med is the storage media to erase. It can be:
+ * \parblock
+ *      -\b MEDIA_INT_FLASH
+ *      .
+ * \endparblock
+ *
+ * \param[in] type is the erase operation type. It can be:
+ * \parblock
+ *      -\b MEDIA_ERASE_DIE
+ *      -\b MEDIA_ERASE_SECTOR
+ *      -\b MEDIA_ERASE_SUB_SECTOR
+ *      .
+ * \endparblock
+ *
+ * \param[in] sector is the sector number to erase.
  *
  * \return The status/error code.
  */
-int media_erase(uint32_t adr);
+int media_erase(media_t med, uint32_t sector);
 
 #endif /* MEDIA_H_ */
 
