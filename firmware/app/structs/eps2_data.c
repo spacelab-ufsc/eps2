408d2e501692072b464f7ca472a539b30d476bd7
Ramon de Araujo Borba <ramonborba97@gmail.com>
2024-09-23 17:20:56 -0300

    firmware: app: structs: Rename time_counter_ms variable to timestamp in the EPS data structure

--- firmware/app/structs/eps2_data.c
+++ firmware/app/structs/eps2_data.c
@@ -64,8 +64,8 @@ int eps_buffer_write(uint8_t id, uint32_t *value)
 {
 	switch(id)
     {
-        case EPS2_PARAM_ID_TIME_COUNTER:
-        	eps_data_buff.time_counter_ms = *value;
+        case EPS2_PARAM_ID_TIMESTAMP:
+        	eps_data_buff.timestamp = *value;
             break;
         case EPS2_PARAM_ID_MCU_TEMP:
             eps_data_buff.eps_mcu_temp_kelvin = *value;
@@ -226,7 +226,7 @@ int eps_buffer_read(uint8_t id, uint32_t *value)
     #if CONFIG_SET_DUMMY_EPS == 1
     switch(id)
     {
-        case EPS2_PARAM_ID_TIME_COUNTER:
+        case EPS2_PARAM_ID_TIMESTAMP:
             *value = 0;
             break;
         case EPS2_PARAM_ID_MCU_TEMP:
@@ -384,8 +384,8 @@ int eps_buffer_read(uint8_t id, uint32_t *value)
 
 	switch(id)
     {
-        case EPS2_PARAM_ID_TIME_COUNTER:
-        	*value = eps_data_buff.time_counter_ms;
+        case EPS2_PARAM_ID_TIMESTAMP:
+        	*value = eps_data_buff.timestamp;
             break;
         case EPS2_PARAM_ID_MCU_TEMP:
             *value = eps_data_buff.eps_mcu_temp_kelvin;
